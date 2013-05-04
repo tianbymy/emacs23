bca47b99c902e4571b640d0b78af174460865194
XieGang <xiegang@zhiyisoft.com>
2013-05-04 08:30:52 +0800

    新增 init.el.bak 配置文件参考

--- init.el
+++ init.el
@@ -2,7 +2,20 @@
 ;;; common config
 ;;; ======================================================================
 (setq load-path (cons "~/.emacs.d/lisp" load-path))
+;;;--------语言环境字符集设置(utf-8)-------------
 
+(set-language-environment 'Chinese-GB)
+(set-keyboard-coding-system 'utf-8)
+(set-clipboard-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-selection-coding-system 'utf-8)
+(modify-coding-system-alist 'process "*" 'utf-8)
+(setq default-process-coding-system '(utf-8 . utf-8))
+(setq-default pathname-coding-system 'utf-8)
+(set-file-name-coding-system 'utf-8)
+(setq ansi-color-for-comint-mode t)
 ;;格式化整个文件函数================================================================
 (defun indent-whole ()
   (interactive)
@@ -10,6 +23,9 @@
   (message "format successfully"))
 ;;绑定到F7键
 (global-set-key [f7] 'indent-whole)
+
+;;设置打开文件的缺省路径
+(setq default-directory "~/work")
 
 ;;font================================================================
 (set-face-attribute
@@ -29,7 +45,7 @@
 (global-set-key [f5] 'refresh-file)
 ;; ------------------------- Settings -------------------------
 (setq default-frame-alist
-      '((height . 40) (width . 120) (menu-bar-lines . 20) (tool-bar-lines . 0))) ;;启动时窗口大小
+      '((top . 0) (left . 10) (height . 40) (width . 120) (menu-bar-lines . 20) (tool-bar-lines . 0))) ;;启动时窗口大小
 (define-key global-map "\C-c\C-t" 'goto-line);;自定义快速跳转到某行
 (define-key global-map "\C-c\C-v" 'ansi-term)
 ;; settings
